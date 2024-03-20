e15018312ee2c8b3e86d1f96b5ac3b3c64cd2718
Marc Angelo <marcangelogilig1@gmail.com>
2024-03-20 23:04:02 +0800

    Nike App Version 1 Unfinished

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import SpecialOffer from "./SpecialOffer";
+import Services from "./Services";
+import CustomerReview from "./CustomerReview";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    SpecialOffer,
+    Services,
+    CustomerReview,
+    Subscribe,
+    Footer
+};
